89a5600e3b3c2f22ee4fe9b31d8522b31be113e6
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-20 15:15:43 +0100

    feat: add a class to handle Polynomial

---
+++ py/Polynome.py
@@ -0,0 +1,55 @@
+class Polynome:
+    def __init__(self, c):
+        self.c = c
+
+    def len(self):
+        return len(self.c)
+
+    def coeff(self, i):
+        if i < self.len():
+            return self.c[i]
+
+        else:
+            return 0
+
+    def vect(self):
+        return self.c
+
+    def img(self, x):
+        for i in range(len(self.c)):
+            """"""
+
+    def __add__(self, Q):
+        Z = []
+
+        for i in range(max(self.len(), Q.len())):
+            Z.append(self.coeff(i) + Q.coeff(i))
+
+        return Polynome(Z)
+
+    def __mul__(self, Q):
+        fl = self.len() + Q.len()
+        Z = []
+
+        for i in range(fl - 1):
+            Z.append(0)
+            for j in range(i + 1):
+                Z[i] += self.coeff(j) * Q.coeff(i - j)
+
+        return Polynome(Z)
+
+    def __sub__(self, Q):
+        Z = []
+
+        for i in range(max(self.len(), Q.len())):
+            Z.append(self.coeff(i) - Q.coeff(i))
+
+        return Polynome(Z)
+
+    def __opp__(self):
+        H = []
+
+        for i in range(self.len()):
+            H.append(-self.coeff(i))
+
+        return Polynome(H)
