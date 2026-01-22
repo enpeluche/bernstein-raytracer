6c6b9d53a35788c59df94048f4352c0d8b8ecf27
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-22 18:47:27 +0100

    doc: remove old doc

--- py/util.py
+++ py/util.py
@@ -1,7 +1,5 @@
 import math
 
-
-# normaliser un vecteur, le mettre de longueur 1
 def normalize3(v):
 
     (x, y, z) = v
@@ -10,24 +8,15 @@ def normalize3(v):
         return (0.0, 0.0, 0.0)
     else:
         return (x / n, y / n, z / n)
-
-
-# produit scalaire
+    
 def pscal3(x1, y1, z1, X):
     (x2, y2, z2) = X
     return x1 * x2 + y1 * y2 + z1 * z2
 
-
-# ???#
 def clamp(mi, ma, v):
     return min(ma, max(mi, v))
 
-
-# ???#
 def interpole(x1, y1, x2, y2, x):
-    # x=x1 -> y=y1
-    # x=x2 -> y=y2
-
     x1, y1, x2, y2, x = float(x1), float(y1), float(x2), float(y2), float(x)
 
     return ((x - x2) / (x1 - x2)) * y1 + ((x - x1) / (x2 - x1)) * y2
