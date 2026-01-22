45a51f21a0d2581d64a60c0bc794da8890b83e0c
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-22 02:16:00 +0100

    changements mineurs

--- py/Polynome.py
+++ py/Polynome.py
@@ -1,26 +1,18 @@
 class Polynome:
-    count__init__ = 0
-    count__len = 0
-    count__len__ = 0
-    count__coeff = 0
-    count__vect = 0
-    count__img = 0
-    count__add__ = 0
-    count__mul__ = 0
-    count__sub__ = 0
-    count__opp__ = 0
-
     def __init__(self, c):
-        Polynome.count__init__ += 1
+
         self.c = c
+
+        while len(self.c) > 1 and self.c[-1] == 0:
+            self.c.pop()
+
         self.len = len(self.c)
 
     def vect(self):
-        Polynome.count__vect += 1
+
         return self.c
 
     def __call__(self, x):
-        Polynome.count__img += 1
 
         n = self.len
 
@@ -32,7 +24,6 @@ class Polynome:
         return value
 
     def __add__(self, Q):
-        Polynome.count__add__ += 1
 
         if self.len > Q.len:
             grand, petit = self, Q
@@ -47,7 +38,6 @@ class Polynome:
         return Polynome(Z)
 
     def __mul__(self, Q):
-        Polynome.count__mul__ += 1
 
         len_self = self.len
         len_Q = Q.len
@@ -66,30 +56,28 @@ class Polynome:
         return Polynome(mul_coeffs)
 
     def __sub__(self, Q):
-        Polynome.count__sub__ += 1
 
-        # Cas A : P est plus long ou égal à Q
         if self.len >= Q.len:
             Z = list(self.c)
-            # On soustrait juste la partie commune
+
             for i in range(Q.len):
                 Z[i] -= Q.c[i]
 
-        # Cas B : Q est plus long que P
         else:
             Z = list(self.c)
-            # 1. On soustrait la partie commune
+
             for i in range(self.len):
                 Z[i] -= Q.c[i]
-            # 2. On ajoute l'opposé de ce qui reste de Q (car 0 - Q[i])
+
             for i in range(self.len, Q.len):
                 Z.append(-Q.c[i])
 
         return Polynome(Z)
 
     def __neg__(self):
-        Polynome.count__opp__ += 1
+
         return Polynome([-c for c in self.c])
 
     def __str__(self):
+
         return f"Polynome({self.c})"
