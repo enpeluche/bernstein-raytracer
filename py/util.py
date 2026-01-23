9c8eda7f3126c75feb9ad575a8abdfc887ce87ed
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-23 14:52:00 +0100

    doc: create docstrings for normalise3, clamp, interpole

--- py/util.py
+++ py/util.py
@@ -1,22 +1,60 @@
 import math
 
-def normalize3(v):
 
-    (x, y, z) = v
-    n = math.sqrt(x * x + y * y + z * z)
-    if 0.0 == n:
+def normalize3(v):
+    """
+    Normalise un vecteur 3D pour lui donner une longueur de 1.
+
+    Args:
+        v (tuple): Un tuple de 3 flottants représentant le vecteur.
+
+    Returns:
+        tuple: Un nouveau tuple de même direction mais de norme 1.
+    """
+
+    (vx, vy, vz) = v
+
+    norm = math.sqrt(vx * vx + vy * vy + vz * vz)
+
+    if 0.0 == norm:
         return (0.0, 0.0, 0.0)
     else:
-        return (x / n, y / n, z / n)
-    
-def pscal3(x1, y1, z1, X):
-    (x2, y2, z2) = X
-    return x1 * x2 + y1 * y2 + z1 * z2
+        return (vx / norm, vy / norm, vz / norm)
 
-def clamp(mi, ma, v):
-    return min(ma, max(mi, v))
+
+# NOTE to clamp : verouiller
+def clamp(m, M, x):
+    """
+    Restreint v à l'intervalle [m, M].
+
+    Args:
+        m (float): La borne inférieure.
+        M (float): La borne supérieure.
+        x (float): La valeur à restreindre.
+
+    Returns:
+        float: La valeur bridée tel que m <= x <= M.
+    """
+    return min(M, max(m, x))
+
 
 def interpole(x1, y1, x2, y2, x):
+    """
+    Calcul l'image de x par interpolation linéaire entre deux points.
+
+    Cette fonction utilise la forme de Lagrange pour un polynôme de degré 1
+    afin de projeter la valeur d'un intervalle source vers un intervalle cible.
+
+    Args:
+        x1 (float): Abscisse du premier point de contrôle.
+        y1 (float): Ordonnée du premier point de contrôle (valeur cible).
+        x2 (float): Abscisse du second point de contrôle.
+        y2 (float): Ordonnée du second point de contrôle (valeur cible).
+        x (float): La valeur à interpoler.
+
+    Returns:
+        float: La valeur interpolée y correspondant à x.
+    """
     x1, y1, x2, y2, x = float(x1), float(y1), float(x2), float(y2), float(x)
 
     return ((x - x2) / (x1 - x2)) * y1 + ((x - x1) / (x2 - x1)) * y2
