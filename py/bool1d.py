e2b5b31cfdc764197bcf4c340a19359268bf1b32
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-25 21:10:24 +0100

    nettoyage et ajout de l'intersection

--- py/bool1d.py
+++ py/bool1d.py
@@ -1,54 +1,66 @@
-def hd(A):
-    # print(A)
-    # (h, t) = A
-    # return h
-    if not A:
-        return None
-    return A[0]
+from Intervalle import Intervalle
 
 
-def tl(A):
-    # (h, t) = A
-    # return t
-    return A[1:]
+def union(A, B):  # A est une liste d'Intervalle
 
-
-def tof(a):
-    return a.t
-
-
-def union(A, B):
-    if None == A:
+    if A == []:
         return B
 
-    elif None == B:
+    if B == []:
         return A
 
+    [a1, a2] = [A[0].a.t, A[0].b.t]
+    [b1, b2] = [B[0].a.t, B[0].b.t]
+
+    assert a1 <= a2
+    assert b1 <= b2
+
+    if a1 > b1:
+        return union(B, A)
+
+    assert a1 <= b1
+
+    # cas disjoint
+    if a2 < b1:
+
+        return [A[0]] + union(A[1:], B)
+
+    assert a2 >= b1
+
+    # si [b1, b2] est inclu dans [a1, a2]
+    if b2 <= a2:
+        return union(A, B[1:])
+
+    assert b2 > a2
+
+    return union([Intervalle(A[0].a, B[0].b)] + A[1:], B[1:])
+
+
+def inter(A, B):
+    if A == [] or B == []:
+        return []
+
+    [a1, a2] = [A[0].a.t, A[0].b.t]
+    [b1, b2] = [B[0].a.t, B[0].b.t]
+
+    assert a1 <= a2
+    assert b1 <= b2
+
+    if a1 > b1:
+        return inter(B, A)
+
+    assert a1 <= b1
+
+    if a2 < b1:  # disjoints
+        return inter(A[1:], B)
+
+    assert a2 >= b1
+
+    if b2 <= a2:  # ya une inclusion
+        return [B[0]] + inter(A, B[1:])
+
     else:
-        (ta, qa) = (hd(A), tl(A))
-        (tb, qb) = (hd(B), tl(B))
-        (a1, a2) = (tof(ta.a), tof(ta.b))
-        (b1, b2) = (tof(tb.a), tof(tb.b))
-
-        assert a1 <= a2
-        assert b1 <= b2
-
-        if a1 > b1:
-            return union(B, A)
-
-        assert a1 <= b1
-
-        # ils ne se touchent pas
-        if a2 < b1:
-            return (ta, (union(tl(A), B)))
-
-        assert a2 >= b1
-
-        if b2 <= a2:
-            return union((ta, tl(A)), tl(B))
-
-        else:
-            return union(union((a1, b2), tl(A)), tl(B))
+        return [Intervalle(B[0].a, A[0].b)] + inter(A[1:], B)
 
 
 def differ(A, B):
@@ -87,76 +99,78 @@ def differ(A, B):
             return ((a1, b1), differ(((b2, a2), tl(A)), tl(B)))
 
 
-def inter(A, B):
-    if None == A or None == B:
-        return None
-    if not A or not B:
-        return None
+def union1D(A, B):
+    print("union1D")
+
+    print(f"A:{A}")
+    print(f"B:{B}")
+
+    if A == []:
+        return B
+
+    if B == []:
+        return A
+
+    [a1, a2] = A[0]
+    [b1, b2] = B[0]
+
+    assert a1 <= a2
+    assert b1 <= b2
+
+    if a1 > b1:
+        return union1D(B, A)
+
+    assert a1 <= b1
+
+    # cas disjoint
+    if a2 < b1:
+        return [[a1, a2]] + union1D(A[1:], B)
+
+    assert a2 >= b1
+
+    # si [b1, b2] est inclu dans [a1, a2]
+    if b2 <= a2:
+        return union1D(A, B[1:])
+
+    assert b2 > a2
+
+    return union1D([[a1, b2]] + A[1:], B[1:])
+
+
+def inter1D(A, B):
+    print("inter1D")
+
+    print(f"A:{A}")
+    print(f"B:{B}")
+
+    if A == [] or B == []:
+        return []
+
+    [a1, a2] = A[0]
+    [b1, b2] = B[0]
+
+    assert a1 <= a2
+    assert b1 <= b2
+
+    if a1 > b1:
+        return inter1D(B, A)
+
+    assert a1 <= b1
+
+    if a2 < b1:  # disjoints
+        return inter1D(A[1:], B)
+
+    assert a2 >= b1
+
+    if b2 <= a2:  # ya une inclusion
+        return [[b1, b2]] + inter1D(A, B[1:])
+
     else:
-        (ta, qa) = (hd(A), tl(A))
-        (tb, qb) = (hd(B), tl(B))
-
-        if ta is None or tb is None:
-            return None
-
-        (a1, a2) = (ta.a.t, ta.b.t)
-        (b1, b2) = (tb.a.t, tb.b.t)
-
-        assert a1 <= a2
-        assert b1 <= b2
-
-        if a1 > b1:
-            return inter(B, A)
-
-        elif a2 < b1:
-            return inter(qa, B)
-
-        elif b2 <= a2:
-            return ((b1, b2), inter(A, qb))
-
-        else:
-            return ((b1, a2), inter(qa, B))
+        return [[b1, a2]] + inter1D(A[1:], B)
 
 
 if __name__ == "__main__":
-    A = [[0, 5]]
-    B = [[3, 8]]
+    A = [[0, 2], [5, 10]]
+    B = [[1, 9]]
 
-    def union1D(A, B):
-        print("union1D")
-
-        print(f"A:{A}")
-        print(f"B:{B}")
-
-        if A == []:
-            return B
-
-        if B == []:
-            return A
-
-        [a1, a2] = A[0]
-        [b1, b2] = B[0]
-
-        assert a1 <= a2  #
-        assert b1 <= b2  #
-
-        if a1 > b1:
-            return union1D(B, A)
-
-        assert a1 <= b1
-
-        # cas disjoint
-        if a2 < b1:
-            return [[a1, a2]] + union1D(A[1:], B)
-
-        assert a2 >= b1
-
-        # si [b1, b2] est inclu dans [a1, a2]
-        if b2 <= a2:
-            return union1D(A, B[1:])
-
-        assert b2 > a2
-
-        return union1D([[a1, b2]] + A[1:], B[1:])
-
-    print(f"\n Résultat: {union1D(A, B)}")
+    print(f"\n Résultat: {inter1D(A, B)}")
