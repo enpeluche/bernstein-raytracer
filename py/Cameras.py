3ea1d2aa6a9cd877709353772985d16d07ede9a6
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-25 13:08:38 +0100

    Ajout de doc, organisation
    Début d'implémentation d'une méthode transform

--- py/Cameras.py
+++ py/Cameras.py
@@ -11,17 +11,17 @@ class Camera:
         self, cam_o, cam_dx, cam_dy, cam_dz, size_world, size_win, light_dir, name
     ):
         """
-        Docstring for __init__
+        Constructeur d'une instance générique de caméra. Ne possède pas la méthode generate_ray.
 
-        :param self: Description
-        :param cam_o: Description
-        :param cam_dx: Description
-        :param cam_dy: Description
-        :param cam_dz: Description
-        :param size_world: Description
-        :param size_win: Description
-        :param light_dir: Description
-        :param name: Description
+        Args:
+            cam_o (tuple[float, float, float]): La position de la caméra.
+            cam_dx (tuple[float, float, float]): La direction de la caméra sur l'axe x.
+            cam_dy (tuple[float, float, float]): La direction de la caméra sur l'axe y.
+            cam_dz (tuple[float, float, float]): La direction de la caméra sur l'axe z.
+            size_world (float): La taille du monde mathématique, peut être flottant.
+            size_win (int): La taille de la fenêtre, en pixel.
+            light_dir (tuple[float, float, float]): La direction de la source de lumière infinie.
+            name (string): Le nom de la caméra.
         """
         self.cam_o = cam_o
         self.cam_dx = cam_dx
@@ -35,11 +35,32 @@ class Camera:
 
     def generate_ray(self, px, pz):
         """
+        Méthode abstraite devant être redéfinie par les sous-classes.
+
+        Args:
+            px (int): L'abscice du pixel que l'on veut calculer.
+            pz (int): L'ordonnée du pixel que l'on veut calculer.
+        """
+        raise NotImplementedError(
+            "La classe de base Camera ne peut pas génerer de rayons."
+        )
+
+    def transform(self, T_o, T_dx, T_dy, T_dz, T_light_dir):
+        """"""
+
+
+class OrthographicCamera(Camera):
+    """
+    OrthographicCamera fixe la direction des rayons, mais fait varier leurs origines.
+    """
+
+    def generate_ray(self, px, pz):
+        """
         Docstring for generate_ray
 
-        :param self: Description
-        :param px: Description
-        :param pz: Description
+        Args:
+            px (int): L'abscice du pixel que l'on veut calculer.
+            pz (int): L'ordonnée du pixel que l'on veut calculer.
         """
         world_x = interpole(0.0, 0.0, self.size_win, self.size_world, float(px))
         world_z = interpole(0.0, 0.0, self.size_win, self.size_world, float(pz))
@@ -52,13 +73,13 @@ class Camera:
                 cam_oy + world_x * self.cam_dx[1] + world_z * self.cam_dz[1],
                 cam_oz + world_x * self.cam_dx[2] + world_z * self.cam_dz[2],
             ),
-            direction=self.oy,
+            direction=self.cam_dy,
         )
 
 
-class CameraPerspective:
+class CameraPerspective(Camera):
     """
-    Docstring for CameraPerspective
+    CameraPerspective simule l'oeil humain en fixant l'origine du rayon mais faisant varier sa direction.
     """
 
     def __init__(
@@ -74,37 +95,30 @@ class CameraPerspective:
         focale=2.0,
     ):
         """
-        Docstring for __init__
+        Constructeur d'une instance de CameraPerspective.
 
-        :param self: Description
-        :param cam_o: Description
-        :param cam_dx: Description
-        :param cam_dy: Description
-        :param cam_dz: Description
-        :param size_world: Description
-        :param size_win: Description
-        :param light_dir: Description
-        :param name: Description
-        :param focale: Description
+        Args:
+            cam_o (tuple[float, float, float]): La position de la caméra.
+            cam_dx (tuple[float, float, float]): La direction de la caméra sur l'axe x.
+            cam_dy (tuple[float, float, float]): La direction de la caméra sur l'axe y.
+            cam_dz (tuple[float, float, float]): La direction de la caméra sur l'axe z.
+            size_world (float): La taille du monde mathématique, peut être flottant.
+            size_win (int): La taille de la fenêtre, en pixel.
+            light_dir (tuple[float, float, float]): La direction de la source de lumière infinie.
+            name (string): Le nom de la caméra.
+            focal (float):
         """
-        self.cam_o = cam_o
-        self.cam_dx = cam_dx
-        self.cam_dy = cam_dy
-        self.cam_dz = cam_dz
-        self.size_world = size_world
-        self.size_win = size_win
-        self.light_dir = normalize3(light_dir)
-        self.background_color = (44, 55, 88)
-        self.name = name
+
+        super().__init__(
+            cam_o, cam_dx, cam_dy, cam_dz, size_world, size_win, light_dir, name
+        )
         self.focale = float(focale)
 
     def generate_ray(self, px, pz):
         """
-        Docstring for generate_ray
-
-        :param self: Description
-        :param px: Description
-        :param pz: Description
+        Args:
+            px (int): L'abscice du pixel que l'on veut calculer.
+            pz (int): L'ordonnée du pixel que l'on veut calculer.
         """
         world_x = interpole(0.0, 0.0, self.size_win, self.size_world, float(px))
         world_z = interpole(0.0, 0.0, self.size_win, self.size_world, float(pz))
