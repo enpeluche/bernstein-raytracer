e3e1f97c2b0f63fe3e6832acd88dd720ef492436
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-25 21:20:45 +0100

    doc: ajout des args

--- py/Intervalle.py
+++ py/Intervalle.py
@@ -1,7 +1,14 @@
-# Intervalle prenant deux contacts
 class Intervalle:
+    """ """
+
     __slots__ = ("a", "b")
 
     def __init__(self, a, b):
+        """
+        Docstring for __init__
+
+            a (RayHit):
+            b (RayHit):
+        """
         self.a = a
         self.b = b
