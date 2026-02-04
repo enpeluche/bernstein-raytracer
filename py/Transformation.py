0441943882f0ffd921225d3e8c16c63be7efec4c
Lucas Petit <lucas.noirotorion@gmail.com>
2026-02-04 13:30:53 +0100

    doc: add a doc for Transformation and transformations functions

--- py/Transformation.py
+++ py/Transformation.py
@@ -3,29 +3,54 @@ from Matrix import Matrix
 
 
 class Transformation:
+    """
+    Représente une transformation de l'espace, contenant la transformation dans un sens, et dans le sens inverse.
+    """
+
     def __init__(self, forward, backward):
-        # Si 'forward' est déjà une Matrix, on la prend telle quelle.
+        """
+        Args:
+            forward (Matrix or tuple[tuple]): La matrice de la transformation (doit être inversible).
+            backward (Matrix or tuple[tuple]): La matrice inverse pré-calculée de 'forward'.
+        """
         if isinstance(forward, Matrix):
             self.forward = forward
         else:
-            # Sinon (c'est une liste), on la convertit.
             self.forward = Matrix(forward)
 
-        # Pareil pour backward
         if isinstance(backward, Matrix):
             self.backward = backward
         else:
             self.backward = Matrix(backward)
 
     def __mul__(self, a):
+        """
+        Surcharge de l'opérateur *.
+        Renvoie la transformation résultante d'effectuer la transformation a puis la transformation self.
+
+        Args:
+            a (Transformation)
+        """
         return Transformation(self.forward * a.forward, a.backward * self.backward)
 
     def __invert__(self):
+        """
+        Surcharge de l'opérateur ~.
+        Retourne sa transformation inverse.
+        """
         return Transformation(self.backward, self.forward)
 
 
 # fmt: off
 def translation(tx, ty, tz):
+    """
+    Retourne une transformation qui effectue une translation de l'objet.
+
+    Args:
+        tx (float): Translation sur l'axe Ox.
+        ty (float): Translation sur l'axe Oy.
+        tz (float): Translation sur l'axe Oz.
+    """
 
     forward = [[1, 0, 0, tx],
                [0, 1, 0, ty],
@@ -41,6 +66,12 @@ def translation(tx, ty, tz):
 
 
 def rotation_x(θ):
+    """
+    Retourne la transformation qui effectue une rotation de l'objet autour de l'axe x.
+
+    Args:
+        θ (float): Angle de rotation en radians.
+    """
 
     forward = [
         [1,      0,       0, 0],
@@ -60,6 +91,12 @@ def rotation_x(θ):
 
 
 def rotation_y(θ):
+    """
+    Retourne la transformation qui effectue une rotation de l'objet autour de l'axe y.
+
+    Args:
+        θ (float): Angle de rotation en radians.
+    """
 
     forward = [
         [ cos(θ), 0, sin(θ), 0],
@@ -78,6 +115,12 @@ def rotation_y(θ):
 
 
 def rotation_z(θ):
+    """
+    Retourne la transformation qui effectue une rotation de l'objet autour de l'axe z.
+
+    Args:
+        θ (float): Angle de rotation en radians.
+    """
 
     forward = [
         [cos(θ), -sin(θ), 0, 0],
@@ -96,7 +139,17 @@ def rotation_z(θ):
 
 
 def scaling(a, b, c):
+    """
+    Retourne la transformation qui étire l'objet.
 
+    Args:
+        a (float): Facteur d'étirement sur l'axe Ox (doit être non nul).
+        b (float): Facteur d'étirement sur l'axe Oy (doit être non nul).
+        c (float): Facteur d'étirement sur l'axe Oz (doit être non nul).
+
+    Raises:
+        ValueError: Si a, b ou c est égal à 0.
+    """
     if a==0 or b==0 or c==0:
         raise ValueError(f"Impossible de créer une affinité avec un facteur nul (a:{a}, b:{b}, c:{c}).")
 
@@ -116,6 +169,9 @@ def scaling(a, b, c):
     return Transformation(forward, backward)
 
 def identity():
+    """
+    Retourne la transformation identité.
+    """
     I = [
         [1, 0, 0, 0],
         [0, 1, 0, 0],
