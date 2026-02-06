27045bbc2309ce3980ce911a05610fb951e73c41
Lucas Petit <lucas.noirotorion@gmail.com>
2026-02-06 17:07:12 +0100

    remove old method, and add doc

--- py/RayHit.py
+++ py/RayHit.py
@@ -9,37 +9,16 @@ class RayHit:
     def __init__(self, t, pt, plan, color):
         """
 
-        :param t: Description
-        :param pt: Description
-        :param plan: Description
-        :param color: Description
+        Args:
+            t (float): Moment t de l'impact.
+            pt tuple[float]: Coordonnées de l'impact.
+            plan tuple[float]: Plan tangeant de l'impact
+            color tuple[float]: Couleur du point d'impact
         """
         self.t = t
         self.pt = pt
         self.plan = plan
         self.color = color
 
-    def transform(self, T):
-        (x, y, z) = self.pt
-        vpt = (x, y, z, 1)
-        [a, b, c, d] = self.plan
-        vplan = [a, b, c, d]
-
-        matvpt = Matrix(vpt)
-        matvplan = Matrix(vplan)
-
-        transfomatvpt = T.forward * matvpt
-        transfomatvplan = T.backward * matvplan
-
-        trf_pt = (
-            transfomatvpt.get(0, 0),
-            transfomatvpt.get(0, 1),
-            transfomatvpt.get(0, 2),
-        )
-        trf_contact = (
-            transfomatvplan.get(0, 0),
-            transfomatvplan.get(0, 1),
-            transfomatvplan.get(0, 2),
-            transfomatvplan.get(0, 3),
-        )
-        return RayHit(contact.t, trf_pt, trf_plan, contact.color)
+    def __repr__(self):
+        return f"RayHit(t={self.t:.3f} pt={self.pt[0]:.3f}, {self.pt[1]:.3f}, {self.pt[2]:.3f}, plan={self.plan[0]:.3f}, {self.plan[1]:.3f}, {self.plan[2]:.3f}, color={self.color})\n"
