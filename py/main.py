d728205946ae4669cb0a83d1a1e84a0404455cd0
Lucas Petit <lucas.noirotorion@gmail.com>
2026-02-27 19:12:13 +0100

    nettoie le main pour tester des démos

--- py/main.py
+++ py/main.py
@@ -1,39 +1,9 @@
-from constants import *
-from Shapes import *
-from Cameras import *
-from Object import *
-from Scene import *
-from Renderer import *
+from demos import *
 
 
-camera = CameraPerspective(
-    cam_o=(0.0, -3, 0.0),
-    cam_dx=(1.0, 0.0, 0.0),
-    cam_dy=(0.0, 1.0, 0.0),  # la direction des rayons aussi
-    cam_dz=(0.0, 0.0, 1.0),
-    size_world=2,
-    size_win=500,
-    light_dir=(0, -1, 1),
-    name="",
-    focale=1.0,
-)
+demo_barillet_anim()
 
-P = Plane(color=(58, 157, 35))
-H1 = HyperboloidOneSheet()
-H2 = HyperboloidTwoSheets()
-R = Roman()
-S = Sphere(1)
-T = Tore(0.2, 1)
-C = Caylay()
-S2 = Steiner2()
-S4 = Steiner4()
 
-P.translate(0, 0, -1)
-W = WhitneyUmbrella(color=(133, 87, 200))
-scene = Scene(camera, W)
-renderer = Renderer(scene)
+# une render distance
 
-for _ in range(180):
-    W.rotate_z(DEG).rotate_y(DEG).rotate_z(DEG)
-    renderer.render()
-renderer.save(format="gif")
+# différent pdv ? pour un format png
